33065073d11b18120bd2ef1c70b2dc926ec77e38
liyufeng <liyufenglyff@163.com>
2018-08-29 18:03:53 +0800

    新增：接口，继承ConfigurableBeanFactory,间接继承BeanFactory,有getBean()并可以配置ClassLoader

---
+++ litespring/src/main/java/lyf/litespring/context/ApplicationContext.java
@@ -0,0 +1,12 @@
+package lyf.litespring.context;
+
+import lyf.litespring.bean.factory.config.ConfigurableBeanFactory;
+
+/**
+ * @Author: liyufeng
+ * @Description:
+ * @Date: Created in 上午11:36 18-8-29
+ * @Modified By:
+ */
+public interface ApplicationContext extends ConfigurableBeanFactory {
+}
